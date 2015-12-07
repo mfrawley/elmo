107825b6f74544d0320548677d845db7c2487c2b
Mark Frawley <markfrawley@gmail.com>
2015-12-07 09:32:48 +0100

    wip

--- modules.elm
+++ modules.elm
@@ -1,11 +1,23 @@
 module Modules where
 import Debug as D
 import Signal exposing (constant)
+import Time exposing (every, second)
 import Graphics.Element exposing (..)
 import Html exposing (span, nav, div, button, text, hr, h1, h2, h3, img, ul, li, a)
 import Html.Attributes exposing (class, style, href)
 
 type HeadingType = H1 | H2 | H3
+
+type alias HeadingPayload = 
+  { h : HeadingType
+  , header : String   
+  }
+
+model : HeadingPayload
+model = 
+  { h = H1
+  , header = ""
+  }
 
 heading : String -> HeadingType -> String 
 heading innerText hType = 
@@ -17,18 +29,29 @@ heading innerText hType =
     H3 ->
       "<h1>" ++ innerText ++ "</h3>"
 
-renderHtml: Signal String 
+renderHtml: String 
 renderHtml =
-     constant (heading "blah" H1) 
+     heading "blah" H1
+
+htmlToElement : String -> Element
+htmlToElement str =
+  show str
+
+port handleSaveAction : Signal String 
+port handleSaveAction =
+  saveModule model
+  "" 
+
+port saveModule : Signal HeadingPayload 
+port saveModule = 
+  model
 
 port renderPort : Signal String
 port renderPort = 
-  renderHtml
-
-htmlToElement str =
-  D.log str
-  show str
+  Signal.map (\_ -> renderHtml) (every second)
 
 main : Signal Element 
 main = 
-  Signal.map htmlToElement renderHtml 
+  let x = renderHtml
+  in
+  Signal.map (\_ -> htmlToElement renderHtml) (every second)
